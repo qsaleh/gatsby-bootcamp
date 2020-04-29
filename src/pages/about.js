9dd69ff8047128c90f2e26889ef7ce92bafc0621
Qusai Saleh <qusaisaleh@Candices-MacBook-Pro.local>
2020-04-28 22:58:42 -0600

    added about and contact pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>I am an Electrical Engineer turned software developer</p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>Contact Info.</h1>
+      <p>I can be contacted at qmsaleh@gmail.com</p>
+    </div>
+  )
+}
+
+export default ContactPage
