484e70d2084ca365ba0233a042638302ceb1d355
Qusai Saleh <qusaisaleh@Candices-MBP.hitronhub.home>
2020-05-09 21:13:20 -0600

    clean up home page links

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,6 @@ const IndexPage = () => {
       <Head title="Home"/>
       <h1>Hello.</h1>
       <h2>I'm Qusai, a full-stack developer living in beautiful Calgary, Alberta</h2>
-      <p>Need a developer? <Link to="/contact">Contact Me!</Link></p>
       </Layout>
     </div>
   )
