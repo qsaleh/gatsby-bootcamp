0e1368df00ca55d9669afe8eabea50de15f8af53
Qusai Saleh <qusaisaleh@Candices-MBP.hitronhub.home>
2020-05-09 18:44:45 -0600

    Rendered contentful posts, not rendering content in posts yet

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,33 +6,29 @@ import blogStyles from './blog.module.scss'
 
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
-  query {
-    allMarkdownRemark {
+  query{
+    allContentfulBlogPost (
+      sort: { fields: publishedDate, order: DESC }){
       edges {
         node {
-          frontmatter {
-            title 
-            date
-          }
-          fields {
-            slug
-          }
+          title
+          slug
+          publishedDate(formatString:"MMMM Do, YYYY")
         }
       }
     }
-    
   }
   `)
   return (
       <Layout>
       <h1>Blog</h1>
       <ol className={blogStyles.posts}>
-        {data.allMarkdownRemark.edges.map((edge) => {
+        {data.allContentfulBlogPost.edges.map((edge) => {
           return (
             <li className={blogStyles.post}>
-              <Link to={`/blog/${edge.node.fields.slug}`}>
-                <h2>{edge.node.frontmatter.title}</h2>
-                <p>{edge.node.frontmatter.date}</p>
+              <Link to={`/blog/${edge.node.slug}`}>
+                <h2>{edge.node.title}</h2>
+                <p>{edge.node.publishedDate}</p>
               </Link>
             </li>
           )
